f5bb7e21dbbeb3f452c243bf2df1d0394cd95da5
soorajk1 <ksooraj162@gmail.com>
2023-10-20 02:17:57 +0530

    feat: added prisma-service, removed everything else

---
+++ src/lib/service-prisma/service-prisma.factory.ts
@@ -0,0 +1,95 @@
+import { join, Path, strings } from "@angular-devkit/core";
+import {
+  apply,
+  branchAndMerge,
+  chain,
+  filter,
+  mergeWith,
+  move,
+  noop,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  template,
+  Tree,
+  url,
+} from "@angular-devkit/schematics";
+import { normalizeToKebabOrSnakeCase } from "../../utils/formatting";
+import {
+  DeclarationOptions,
+  ModuleDeclarator,
+} from "../../utils/module.declarator";
+import { ModuleFinder } from "../../utils/module.finder";
+import { Location, NameParser } from "../../utils/name.parser";
+import { mergeSourceRoot } from "../../utils/source-root.helpers";
+import { ServiceOptions } from "./service-prisma.schema";
+
+export function main(options: ServiceOptions): Rule {
+  options = transform(options);
+  return (tree: Tree, context: SchematicContext) => {
+    return branchAndMerge(
+      chain([
+        mergeSourceRoot(options),
+        addDeclarationToModule(options),
+        mergeWith(generate(options)),
+      ])
+    )(tree, context);
+  };
+}
+
+function transform(source: ServiceOptions): ServiceOptions {
+  const target: ServiceOptions = Object.assign({}, source);
+  target.metadata = "providers";
+  target.type = "service";
+
+  if (target.name === null || target.name === undefined) {
+    throw new SchematicsException("Option (name) is required.");
+  }
+
+  const location: Location = new NameParser().parse(target);
+  target.name = normalizeToKebabOrSnakeCase(location.name);
+  target.path = normalizeToKebabOrSnakeCase(location.path);
+  target.path = target.flat
+    ? target.path
+    : join(target.path as Path, target.name);
+  return target;
+}
+
+function generate(options: ServiceOptions) {
+  return (context: SchematicContext) =>
+    apply(url(join("./files" as Path)), [
+      options.spec
+        ? noop()
+        : filter((path) => {
+            const suffix = `.__specFileSuffix__.ts`;
+            return !path.endsWith(suffix);
+          }),
+      template({
+        ...strings,
+        ...options,
+      }),
+      move(options.path),
+    ])(context);
+}
+
+function addDeclarationToModule(options: ServiceOptions): Rule {
+  return (tree: Tree) => {
+    if (options.skipImport !== undefined && options.skipImport) {
+      return tree;
+    }
+    options.module = new ModuleFinder(tree).find({
+      name: options.name,
+      path: options.path as Path,
+    });
+    if (!options.module) {
+      return tree;
+    }
+    const content = tree.read(options.module).toString();
+    const declarator: ModuleDeclarator = new ModuleDeclarator();
+    tree.overwrite(
+      options.module,
+      declarator.declare(content, options as DeclarationOptions)
+    );
+    return tree;
+  };
+}
