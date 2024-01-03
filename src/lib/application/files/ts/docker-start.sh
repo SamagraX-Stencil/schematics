4c9cf1b8671078ef803cec85c2d3170d983107b5
sooraj1002 <ksooraj162@gmail.com>
2024-01-03 22:25:15 +0530

    feat: simultaneously start containers

---
+++ src/lib/application/files/ts/docker-start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+docker compose up -d
+
+LOGGING_DIR="./services/logging"
+
+if [ -d "$LOGGING_DIR" ]; then
+
+  cd "$LOGGING_DIR"
+  
+  docker compose up -d
+
+  cd ../..
+
+fi
+
+MONITOR_DIR="./services/monitor"
+
+if [ -d "$MONITOR_DIR" ]; then
+  cd "$MONITOR_DIR"
+  
+  docker compose up -d
+
+  cd ../..
+
+fi
+
+TEMPORAL_DIR="./services/temporal"
+
+if [ -d "$TEMPORAL_DIR" ]; then
+
+  cd "$TEMPORAL_DIR"
+  
+  docker compose up -d
+
+  cd ../..
+
+fi
