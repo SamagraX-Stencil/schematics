c87faa221409cee78c24fdbf47469d243f940c56
Savio Dias <91362589+Savio629@users.noreply.github.com>
2024-04-09 08:20:02 +0000

    fix: multiple imports from custom package

--- src/utils/module-import.declarator.ts
+++ src/utils/module-import.declarator.ts
@@ -9,10 +9,35 @@ export class ModuleImportDeclarator {
     const toInsert = this.buildLineToInsert(options);
     const contentLines = content.split('\n');
     const finalImportIndex = this.findImportsEndpoint(contentLines);
-    contentLines.splice(finalImportIndex + 1, 0, toInsert);
-    return contentLines.join('\n');
-  }
+    const updatedContent = this.mergeImportStatements(contentLines, finalImportIndex, toInsert,options);
+    return updatedContent.join('\n');
+}
 
+private mergeImportStatements(contentLines: string[], finalImportIndex: number, toInsert: string, options: DeclarationOptions): string[] {
+  const existingImportLineIndex = contentLines.findIndex(line =>
+    line.startsWith(`import {`) && line.includes(`} from '${options.path}';`)
+  );
+
+  if (existingImportLineIndex !== -1) {
+    const existingImportLine = contentLines[existingImportLineIndex];
+    const startIndex = existingImportLine.indexOf('{') + 2;
+    const endIndex = existingImportLine.lastIndexOf('}');
+
+    const existingSymbols = existingImportLine.substring(startIndex, endIndex).trim().split(',').map(symbol => symbol.trim());
+    const newSymbols = toInsert.substring(toInsert.indexOf('{') + 1, toInsert.lastIndexOf('}')).trim().split(',').map(symbol => symbol.trim());
+    
+    const mergedSymbols = [...new Set(existingSymbols.concat(newSymbols))]; 
+    
+    const updatedImportLine = existingImportLine.substring(0, startIndex) +
+                              mergedSymbols.join(', ') +
+                              existingImportLine.substring(endIndex);
+
+    contentLines[existingImportLineIndex] = updatedImportLine;
+  } else {
+    contentLines.splice(finalImportIndex + 1, 0, toInsert);
+  }
+  return contentLines;
+}
   private findImportsEndpoint(contentLines: string[]): number {
     const reversedContent = Array.from(contentLines).reverse();
     const reverseImports = reversedContent.filter((line) =>
