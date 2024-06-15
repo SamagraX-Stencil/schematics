50c2171ffd800b33571eef803bcd9a0ecf7b1ce3
shiv <shiv@kali.shivlaptop>
2024-06-15 15:05:17 +0530

    fix: changes updated

--- src/lib/husky/pre-commit.ts
+++ src/lib/husky/pre-commit.ts
@@ -1,7 +1,15 @@
 export const preCommit = `#!/usr/bin/env sh
 . "\$(dirname -- "\$0")/_/husky.sh"
 
-npm run lint
+# Run Prettier to check and fix code formatting
+npm run prettier 
+
+# Run ESLint to check and fix code issues
+npm run lint 
+
+# Stage changes made by Prettier and ESLint
+git add .
+
 docker compose -f docker-compose.ci.yaml --env-file env-example -p ci up --build --exit-code-from api
 # npm test
 # npm test:e2e -- --runInBank --passWithNoTests`;
